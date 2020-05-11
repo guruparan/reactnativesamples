21097fdd83270fcdb96adda99fb7b16d0c1b932d
Guruparan Giritharan <guru_sig@yahoo.com>
2020-05-11 09:03:47 +0530

    Appcenter integration

--- ios/MoviesApp/AppDelegate.m
+++ ios/MoviesApp/AppDelegate.m
@@ -6,8 +6,10 @@
  */
 
 #import "AppDelegate.h"
-#import <CodePush/CodePush.h>
 
+#import <AppCenterReactNative.h>
+#import <AppCenterReactNativeAnalytics.h>
+#import <AppCenterReactNativeCrashes.h>
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
